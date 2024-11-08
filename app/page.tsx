4918b891c40bbb9f864b38cdf19554cfc4009f06
Naufal Adriansyah <naufaladrn90@gmail.com>
2024-11-08 19:48:45 +0700

    Membuat dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 
 const Home = () => {
+  const [nama, setNama] = useState("Rizky");
+  const [nomor, setNomor] = useState(100);
+
   return (
-    <h1 className="text-3xl text-white font-black">Welcome world Next JS</h1>
+    <div className="flex flex-col gap-4 m-8 w-1/4">
+      <h1 className="h1-bold background-light850_dark100">
+        Welcome of the world Next.jS tailwind css is FUN!
+      </h1>
+
+      <input
+        type="text"
+        value={nama}
+        onChange={(e) => setNama(e.target.value)}
+        className="p-2 rounded-lg"
+      />
+
+      <button className="bg-blue-500 text-white p-2 rounded-lg">Update</button>
+    </div>
   );
 };
 
